33b95457128d179b327f284cacc0e0391ff44b6a
Yenni Delgado <yennivanessadelgado@gmail.com>
2026-01-07 15:41:07 -0500

    Add MobileMenu section

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,21 @@ import { LoadingScreen } from './components/LoadingScreen'
 import "./index.css"
 import { Navbar } from './components/Navbar'
 import { useState } from 'react'
+import { MobileMenu } from './components/MobileMenu'
 
 
 function App() {
 
   const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
 
   return <>
 
   {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)}/>} <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? 'opacity-100' : 'opacity-0'} bg-black text-gray-100`}>
 
-  <Navbar />
+  <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+  <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
   </div>
 
   </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,59 @@
+import { useEffect } from "react";
 
-export const Navbar = () => {
-    return <nav className="fixed top-0 w-full z-40 bg-[rgba(10,10,10,0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg  ">
-
-    <div className="max-w-5xl max-auto px-4">
+export const Navbar = ({ menuOpen, setMenuOpen }) => {
+  useEffect(() => {
+    document.body.style.overflow = menuOpen ? "hidden" : "";
+  }, [menuOpen]);
+  return (
+    <nav className="fixed top-0 w-full z-40 bg-[rgba(10, 10, 10, 0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
+      <div className="max-w-5xl mx-auto px-4">
         <div className="flex justify-between items-center h-16">
-            <a href="#home" className="font-mono text-xl font-bold text-white">
-                {""}
+          <a href="#home" className="font-mono text-xl font-bold text-white">
+            {" "}
+            Yenni<span className="text-[#D7B0FF] drop-shadow-[0_0_6px_rgba(215,176,255,0.6)]">
+             | Software & Data
+            </span> {" "}
+          </a>
 
-                Yenni<span className="text-[#B98BFF] drop-shadow-[0_0_6px_rgba(215,176,255,0.6)]">| Software & Data</span>{""}
+          <div
+            className="w-7 h-5 relative cursor-pointer z-40 md:hidden"
+            onClick={() => setMenuOpen((prev) => !prev)}
+          >
+            &#9776;
+          </div>
 
-
+          <div className="hidden md:flex items-center space-x-8">
+            <a
+              href="#home"
+              className="text-gray-300 hove:text-white transition-colors"
+            >
+              
+              Home
             </a>
+            <a
+              href="#about"
+              className="text-gray-300 hove:text-white transition-colors"
+            >
+              
+              About
+            </a>
+            <a
+              href="#projects"
+              className="text-gray-300 hove:text-white transition-colors"
+            >
+              
+              Projects
+            </a>
+            <a
+              href="#contact"
+              className="text-gray-300 hove:text-white transition-colors"
+            >
+              
+              Contact
+            </a>
+          </div>
         </div>
-
-    </div>
-
+      </div>
     </nav>
-
-}
+  );
+};
