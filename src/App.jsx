4b80428d63ed28430342f97f57e87c204470d59c
Yenni Delgado <yennivanessadelgado@gmail.com>
2026-01-07 13:06:20 -0500

    Add navbar incompleted

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import { LoadingScreen } from './components/LoadingScreen'
 import "./index.css"
+import { Navbar } from './components/Navbar'
 import { useState } from 'react'
 
 
@@ -11,7 +12,10 @@ function App() {
 
   return <>
 
-  {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)}/>}
+  {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)}/>} <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? 'opacity-100' : 'opacity-0'} bg-black text-gray-100`}>
+
+  <Navbar />
+  </div>
 
   </>
     
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+
+export const Navbar = () => {
+    return <nav className="fixed top-0 w-full z-40 bg-[rgba(10,10,10,0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg  ">
+
+    <div className="max-w-5xl max-auto px-4">
+        <div className="flex justify-between items-center h-16">
+            <a href="#home" className="font-mono text-xl font-bold text-white">
+                {""}
+
+                Yenni<span className="text-[#B98BFF] drop-shadow-[0_0_6px_rgba(215,176,255,0.6)]">| Software & Data</span>{""}
+
+
+            </a>
+        </div>
+
+    </div>
+
+    </nav>
+
+}
